1ad82be49e3dc583bd81a8b24591f7dbed18fe15
Arne Jans <arne.jans@gmail.com>
2010-10-15 16:19:21 +0200

    first version of readonly-tasks for producteev-teammember-tasks

--- src/com/todoroo/astrid/data/Task.java
+++ src/com/todoroo/astrid/data/Task.java
@@ -14,11 +14,11 @@ import android.net.Uri;
 
 import com.todoroo.andlib.data.AbstractModel;
 import com.todoroo.andlib.data.Property;
-import com.todoroo.andlib.data.Table;
-import com.todoroo.andlib.data.TodorooCursor;
 import com.todoroo.andlib.data.Property.IntegerProperty;
 import com.todoroo.andlib.data.Property.LongProperty;
 import com.todoroo.andlib.data.Property.StringProperty;
+import com.todoroo.andlib.data.Table;
+import com.todoroo.andlib.data.TodorooCursor;
 import com.todoroo.andlib.utility.DateUtilities;
 import com.todoroo.astrid.api.AstridApiConstants;
 import com.todoroo.astrid.api.R;
@@ -139,6 +139,9 @@ public final class Task extends AbstractModel {
 
     /** whether repeat occurs relative to completion date instead of due date */
     public static final int FLAG_REPEAT_AFTER_COMPLETION = 1 << 1;
+
+    /** whether task is readonly */
+    public static final int FLAG_IS_READONLY = 1 << 2;
 
     // --- notification flags
 
